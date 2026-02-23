52ba672cb0b8a47d2ed67a4d12b366d1a902c85b
Nuno Lopes <nblopes@learninghubz.com>
2026-02-23 18:13:01 +0000

    fix: add triple-redundant redirect (inline script, meta refresh, React router)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import {Redirect} from '@docusaurus/router';
 import Head from '@docusaurus/Head';
+import {useEffect} from 'react';
 
 export default function Home() {
+  // Immediate redirect via JavaScript
+  useEffect(() => {
+    window.location.replace('/lhz.docs/docs/intro');
+  }, []);
+
   return (
     <>
       <Head>
         <meta httpEquiv="refresh" content="0; url=/lhz.docs/docs/intro" />
+        <script dangerouslySetInnerHTML={{
+          __html: `window.location.replace('/lhz.docs/docs/intro');`
+        }} />
       </Head>
       <Redirect to="/docs/intro" />
     </>
